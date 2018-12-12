c7d26ebf440276231fd3fc86778f2478ba2ab5cd
Klaus <klausheina@gmail.com>
2018-12-12 11:02:19 -0600

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HandItDown.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HandItDown.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+
+        {
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace HandItDown.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser() { }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+      
+
+
+
+        // Set up PK -> FK relationships to other objects
+        // public virtual ICollection<Product> Products { get; set; }
+
+        // public virtual ICollection<Order> Orders { get; set; }
+
+        // public virtual ICollection<PaymentType> PaymentTypes { get; set; }
+    }
+}
